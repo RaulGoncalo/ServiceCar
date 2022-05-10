38c32de7889e9aa20a2e510d06df28aef18f2847
Raul Goncalo <raulgoncalo.98@gmail.com>
2022-05-10 20:08:40 -0300

    Salvando alterações - 10/05/2022

--- index.js
+++ index.js
@@ -4,5 +4,6 @@
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
+import {name as appName} from './app.json';
 
-AppRegistry.registerComponent('app', () => App);
+AppRegistry.registerComponent(appName, () => App);
